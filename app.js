d4e1bbc24203d178be16b9f7256491c2d8b45981
varadiistvan <varadiista.1@gmail.com>
2021-01-13 08:14:39 +0100

    hehhhehe

--- app.js
+++ app.js
@@ -8,19 +8,15 @@ app.use(cors())
 
 
 
-var teams = {"team1": [], "team2": [], "team3": [], "team4": [], "team5": []}
+var teams = {"team1": {"players": [], "points": 0}, "team2": {"players": [], "points": 0}, "team3": {"players": [], "points": 0}, "team4": {"players": [], "points": 0}, "team5": {"players": [], "points": 0}}
 
 var board = [[],[],[],[],[],[],[]]
 
 
+var turn = ""
+var chosen = false
 
-
-
-
-
-
-
-
+var time = new Date().getSeconds()
 
 
 app.get("/", (req, res) => {
@@ -30,20 +26,72 @@ app.get("/", (req, res) => {
 })
 
 app.get("/getAll", (req, res) => {
-    console.log(req.ip);
     let response = {}
+    console.log(req.ip);
     response.teams = teams
     response.board = [["Sure", "második téma", "Génmódosítás", "negyedik téma", "ötödik téma", "hatodik téma", "hetedik téma"],[{"kérdés": "valaminagyonhosszú de tényleg, mintha igazi kérdés lenne", "status": "claimed"},{"kérdés": "Egy fajon belül adott génállományú populáció egy vagy több tagja bekerül egy másik populációba, ezzel megváltoztatva annak génállományát.	", "status": "current"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"}],[{"kérdés": "Egy fajon belül adott génállományú populáció egy vagy több tagja bekerül egy másik populációba, ezzel megváltoztatva annak génállományát.	", "status": "claimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"}],[{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"}],[{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"}],[{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"}],[{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"},{"kérdés": "valami", "status": "unclaimed"}]]
+    response.currentTeam = turn
+    if(chosen) {
+        response.timeStamp = time
+        // console.log(time);
+    }
     res.send(response)
+})
+
+app.put("/choosenext", (req, res) => {
+    if(req.body.team == turn && !chosen) {
+        chosen = true
+        // console.log(parseInt(req.body.target[1]));
+        // console.log(parseInt(req.body.target[3]));
+        console.log(board[0][2])
+        time = new Date().getSeconds()
+        setTimeout(() => time = false, 900)
+    }
+    else{
+        res.send()
+    }
+    
 })
 
 
 app.post("/newPlayer", (req, res) => {
-    teams[req.body.team].push(req.body.name)
+    teams[req.body.team].players.push(req.body.name)
     res.send()
     console.log(teams)
 })
 
+
+
+app.get("/newTurn", (req, res) => {
+    chosen = false
+    switch(turn) {
+        case "":
+            turn = "team1"
+            break
+        
+        case "team1":
+            turn = "team2"
+            break
+        
+        case "team2":
+            turn = "team3"
+            break
+        
+        case "team3":
+            turn = "team4"
+            break
+        
+        case "team4":
+            turn = "team5"
+            break
+
+        case "team5":
+            turn = "team1"
+            break
+    }
+    console.log(turn);
+    res.send()
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("started on " + port))
